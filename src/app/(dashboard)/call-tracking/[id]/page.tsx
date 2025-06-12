1331ff4db3aa8836c4fd7013d392a57be1f0f76a
AbyerAliWanile <ali.abyer@wanile.com>
2025-06-12 14:32:48 +0500

    fixing the TS errors

--- src/app/(dashboard)/call-tracking/[id]/page.tsx
+++ src/app/(dashboard)/call-tracking/[id]/page.tsx
@@ -2,18 +2,10 @@
 
 import { CallDetail } from '../components/call-details';
 
-// Define the expected props shape using an interface. This is the standard practice.
-interface CallDetailPageProps {
-  params: {
-    id: string;
-  };
-}
-
-// Use the interface to type your component's props.
-export default function CallDetailPage({ params }: CallDetailPageProps) {
+export default function CallDetailPage() {
 	return (
 		<main className="sm:p-6 p-3 bg-[#f9f8fa]">
-			<CallDetail callId={params.id} />
+			<CallDetail callId={'234'} />
 		</main>
 	);
 }
